2a072028994a55b53e4999d630a414d5def9c140
soeguet <osman_s@live.de>
2024-04-14 18:05:46 +0200

    add: regex cases for inputfield

--- gui.go
+++ gui.go
@@ -112,6 +112,7 @@ func checkForReactions(reactionType []ReactionType) string {
 	return reactions.String()
 }
 
+// checks for [000] > or [000] >> in the message
 func evalTextInChatView(text string) int {
 
 	regexPattern := `^\[([0-9]{3})\] (>{1,2}) `
@@ -134,6 +135,10 @@ func evalTextInChatView(text string) int {
 	}
 }
 
+// createInputField creates a new tview.InputField with a specific label, label color, and field background color.
+// It also sets up event handlers for text changes and when the enter key is pressed.
+// The input field's field text color is set based on the evaluation of the input text using evalTextInChatView.
+// It returns the created input field.
 func createInputField(app *App) *tview.InputField {
 
 	inputField := tview.NewInputField()
@@ -143,33 +148,82 @@ func createInputField(app *App) *tview.InputField {
 
 	inputField.SetChangedFunc(func(text string) {
 		textCase := evalTextInChatView(text)
+
+		if textCase == 0 {
+			textCase = evalTextInChatViewV2(text)
+		}
+
+		if textCase == 0 {
+			textCase = evalTextInChatViewV3(text)
+		}
+
 		switch textCase {
 		case 1:
 			// quote
+			inputField.SetFieldBackgroundColor(tcell.ColorDarkSlateGray)
 			inputField.SetFieldTextColor(tcell.ColorDarkOrange)
 		case 2:
 			// reaction
+			inputField.SetFieldBackgroundColor(tcell.ColorDarkSlateGray)
 			inputField.SetFieldTextColor(tcell.ColorGreen)
+		case 3:
+			// settings change
+			inputField.SetFieldBackgroundColor(tcell.ColorDarkSlateGray)
+			inputField.SetFieldTextColor(tcell.ColorYellow)
 		default:
+			inputField.SetFieldBackgroundColor(tcell.ColorBlueViolet)
 			inputField.SetFieldTextColor(tcell.ColorWhite)
 		}
 	})
 
 	inputField.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
-			textInputField := inputField.GetText()
-			textCase := evalTextInChatView(textInputField)
-			switch textCase {
-			case 1:
-				sendQuotedMessagePayloadToWebsocket(app.conn, &textInputField)
-			// quote
-			case 2:
-				sendReactionPayloadToWebsocket(app.conn, &textInputField)
-			// reaction
-			default:
-				// plain message
-				sendMessagePayloadToWebsocket(app.conn, &textInputField)
+
+			textInput := inputField.GetText()
+
+			// check for [000] > or [000] >> in the message
+			textCase := evalTextInChatView(textInput)
+			if textCase == 0 {
+				textCase = evalTextInChatViewV2(textInput)
 			}
+
+			if textCase == 0 {
+				textCase = evalTextInChatViewV3(textInput)
+			}
+
+			if textCase != 0 {
+				switch textCase {
+				case 1:
+					// quote
+					sendQuotedMessagePayloadToWebsocket(app.conn, &textInput)
+				case 2:
+					// reaction
+					sendReactionPayloadToWebsocket(app.conn, &textInput)
+				case 3:
+					// settings change
+					sendProfileUpdateToWebsocket(app.conn, &textInput)
+				default:
+					// plain message
+					sendMessagePayloadToWebsocket(app.conn, &textInput)
+				}
+			}
+
+			// check for /q or /r followed by three digits and a space
+			textCaseV2 := evalTextInChatViewV2(textInput)
+			if textCaseV2 != 0 {
+				switch textCaseV2 {
+				case 1:
+					sendQuotedMessagePayloadToWebsocketV2(app.conn, &textInput)
+				// quote
+				case 2:
+					sendReactionPayloadToWebsocketV2(app.conn, &textInput)
+				// reaction
+				default:
+					// plain message
+					sendMessagePayloadToWebsocket(app.conn, &textInput)
+				}
+			}
+
 			inputField.SetText("")
 		}
 	})
@@ -177,6 +231,87 @@ func createInputField(app *App) *tview.InputField {
 	return inputField
 }
 
+func sendProfileUpdateToWebsocket(conn *websocket.Conn, message *string) {
+
+	// schema: /s:
+
+	// grab characters in brackets
+	trimmedMessageIndex := (*message)[1:4]
+	reactedMessagePayload := GetMessageFromCache(atoi(trimmedMessageIndex))
+	// remove the first 8 characters
+	trimmedMessage := (*message)[8:]
+
+	reactionPayload := ReactionPayload{
+		PayloadType:       7,
+		ReactionDbId:      uuid.New().String(),
+		ReactionMessageId: reactedMessagePayload.MessageType.MessageDbId,
+		ReactionContext:   trimmedMessage,
+		ReactionClientId:  envVars.Id,
+	}
+
+	err := conn.WriteJSON(reactionPayload)
+	if err != nil {
+		fmt.Println("Error writing messagePayload:", err)
+	}
+
+}
+
+// checks for /q or /r followed by three digits and a space
+func evalTextInChatViewV2(text string) int {
+	regexPattern := `^/(q|r)[0-9]{3} `
+	re := regexp.MustCompile(regexPattern)
+
+	matches := re.FindStringSubmatch(text)
+	if matches != nil {
+		switch matches[1] {
+		case "q":
+			return 1
+		case "r":
+			return 2
+		default:
+			return 0
+		}
+	} else {
+		return 0
+	}
+}
+
+// checks for /s followed by three digits and a space
+func evalTextInChatViewV3(text string) int {
+	regexPattern := `^/s[n,c]: `
+	re := regexp.MustCompile(regexPattern)
+
+	matches := re.FindStringSubmatch(text)
+	if matches != nil {
+		return 3
+	} else {
+		return 0
+	}
+}
+func sendReactionPayloadToWebsocketV2(conn *websocket.Conn, message *string) {
+
+	// schema: /r005
+
+	// grab characters in brackets
+	trimmedMessageIndex := (*message)[2:5]
+	reactedMessagePayload := GetMessageFromCache(atoi(trimmedMessageIndex))
+	// remove the first 6 characters
+	trimmedMessage := (*message)[6:]
+
+	reactionPayload := ReactionPayload{
+		PayloadType:       7,
+		ReactionDbId:      uuid.New().String(),
+		ReactionMessageId: reactedMessagePayload.MessageType.MessageDbId,
+		ReactionContext:   trimmedMessage,
+		ReactionClientId:  envVars.Id,
+	}
+
+	err := conn.WriteJSON(reactionPayload)
+	if err != nil {
+		fmt.Println("Error writing messagePayload:", err)
+	}
+
+}
 func sendReactionPayloadToWebsocket(conn *websocket.Conn, message *string) {
 
 	// schema: [000] >>
@@ -202,6 +337,42 @@ func sendReactionPayloadToWebsocket(conn *websocket.Conn, message *string) {
 
 }
 
+func sendQuotedMessagePayloadToWebsocketV2(conn *websocket.Conn, message *string) {
+
+	// schema: /q000
+
+	// grab characters in brackets
+	trimmedMessageIndex := (*message)[2:5]
+	quotedMessagePayload := GetMessageFromCache(atoi(trimmedMessageIndex))
+	// remove the first 5 characters
+	trimmedMessage := (*message)[5:]
+
+	messagePayload := MessagePayload{
+		PayloadType: 1,
+		MessageType: MessageType{
+			MessageDbId:    "TOBEREMOVED",
+			MessageContext: base64.StdEncoding.EncodeToString([]byte(trimmedMessage)),
+			MessageTime:    time.Now().Format("15:04"),
+			MessageDate:    time.Now().Format("2006-01-02"),
+		},
+		ClientType: ClientType{
+			ClientDbId: envVars.Id,
+		},
+		QuoteType: &QuoteType{
+			QuoteDbId:           quotedMessagePayload.MessageType.MessageDbId,
+			QuoteClientId:       quotedMessagePayload.ClientType.ClientDbId,
+			QuoteMessageContext: quotedMessagePayload.MessageType.MessageContext,
+			QuoteTime:           quotedMessagePayload.MessageType.MessageTime,
+			QuoteDate:           quotedMessagePayload.MessageType.MessageDate,
+		},
+		ReactionType: nil,
+	}
+
+	err := conn.WriteJSON(messagePayload)
+	if err != nil {
+		fmt.Println("Error writing messagePayload:", err)
+	}
+}
 func sendQuotedMessagePayloadToWebsocket(conn *websocket.Conn, message *string) {
 
 	// schema: [000] >
